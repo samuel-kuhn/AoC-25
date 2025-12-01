084e5953c30bb28d54e85c4dcabb63a3a2a83a44
Samuel Kuhn <samuel.kuhn@yoga>
2025-12-01 23:03:28 +0100

    first implementation of method click returns a too high number

--- Day1/main.py
+++ Day1/main.py
@@ -1,3 +1,6 @@
+import time
+
+
 class Rotation:
   def __init__(self, direction, count):
     self.direction = direction
@@ -19,23 +22,48 @@ def parse(rotation: str):
 with open("input.txt", "r") as f:
   rotations = f.readlines()
 
-zero_count = 0
-dial = 50
 
-for r_string in rotations:
-  rotation = parse(r_string)
-  if rotation.direction == 'L':
-    dial += 100
-    dial -= rotation.count
-  elif rotation.direction == 'R':
-    dial += rotation.count
-  else:
-    raise Exception("Unknown action: " + rotation.direction)
+def method1():
+  zero_count = 0
+  dial = 50
+  for r_string in rotations:
+    rotation = parse(r_string)
+    if rotation.direction == 'L':
+      dial += 100
+      dial -= rotation.count
+    elif rotation.direction == 'R':
+      dial += rotation.count
+    else:
+      raise Exception("Unknown action: " + rotation.direction)
 
-  dial = dial % 100
-  if dial == 0:
-    zero_count += 1
+    dial = dial % 100
+    if dial == 0:
+      zero_count += 1
+  return zero_count
 
-print(f"Dial was at 0 {zero_count} times.")
+def method_CLICK():
+  zero_count = 0
+  dial = 50
+  for r_string in rotations:
+    rotation = parse(r_string)
+    if rotation.direction == 'L':
+      dial -= rotation.count
+    elif rotation.direction == 'R':
+      dial += rotation.count
+    else:
+      raise Exception("Unknown action: " + rotation.direction)
+
+    while dial < 0 or dial > 99:
+      if dial < 0:
+        dial += 100
+        zero_count += 1
+      if dial > 99:
+        dial -= 100
+        zero_count += 1
+  return zero_count
+
+if __name__ == "__main__":
+  print("Method 1 result: " + str(method1()))
+  print("Method CLICK result: " + str(method_CLICK()))
 
 
